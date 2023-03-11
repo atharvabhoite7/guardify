e477842ab900ff0d57362bfbe6bcfd3e4e22a735
Shreyansh Singh <2020.shreyansh.singh@ves.ac.in>
2023-03-11 10:28:35 +0530

    added locator

--- frontend/components/Sidebar/Sidebar.js
+++ frontend/components/Sidebar/Sidebar.js
@@ -161,6 +161,29 @@ export default function Sidebar() {
                 </Link>
               </li>
 
+              <li className="items-center">
+                <Link href="/cybercell">
+                  <a
+                    className={
+                      "text-sm uppercase py-3 font-bold block " +
+                      (router.pathname.indexOf("/cybercell") !== -1
+                        ? "text-lightBlue-500 hover:text-lightBlue-600"
+                        : "text-blueGray-700 hover:text-blueGray-500")
+                    }
+                  >
+                    <i
+                      className={
+                        "fas fa-tv mr-2 text-sm " +
+                        (router.pathname.indexOf("/cybercell") !== -1
+                          ? "opacity-75"
+                          : "text-blueGray-300")
+                      }
+                    ></i>{" "}
+                    CyberCell Locator
+                  </a>
+                </Link>
+              </li>
+
               {/* <li className="items-center">
                 <Link href="/crop">
                   <a
